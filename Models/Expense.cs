2c28e0fb6d2510c91ba8803ca75ec8617e42ea7b
arm <armshare.it@gmail.com>
2025-09-07 11:24:33 +0700

    Day 6:Added model validation for Expense

--- Models/Expense.cs
+++ Models/Expense.cs
@@ -1,11 +1,16 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace ExpenseTracker.Api.Models
 {
     public class Expense
     {
         public int Id { get; set; }
+        [Required]
         public DateTime Date { get; set; }
+        [Required]
         public string Category { get; set; }
+        [Range(0.01, 100000)]
         public decimal Amount { get; set; }
         public string? Note { get; set; }
     }
